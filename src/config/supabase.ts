9a43ff720d4db19dfad7ce2fee1bf4266af74dfe
felipin977 <fellipemsv@gmail.com>
2025-05-29 14:32:26 -0300

    Updated supabase.ts

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
+const supabaseUrl = 'https://vusdncuduzoudxrimdjs.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1c2RuY3VkdXpvdWR4cmltZGpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MzkwNTQsImV4cCI6MjA2NDExNTA1NH0.SLp5pYDXO7Lk7Oqzwb_G2piCSf9e6e4fExCpYwK074A';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
